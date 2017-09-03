e36772d87ffc1ff9ff1541862b1056bd08258fc3
Brian Nguyen <thuann89@gmail.com>
2017-09-03 11:42:03 +0700

    Add form for messages and show incoming message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,27 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('newMessage', message);
+    var wrap = document.getElementById('messages');
+    var messageElm = document.createElement('li');
+    var text = document.createTextNode(message.from+': '+message.text); 
+    messageElm.appendChild(text);
+    wrap.appendChild(messageElm);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hello'
+// }, function (data) {
+//     console.log('got it', data);
+// });
+
+document.getElementById('message-form').addEventListener('submit',function (e) {
+    e.preventDefault();
+    var text = document.getElementById('messageInput').value;
+    socket.emit('createMessage', {
+        from: 'User',
+        text
+    }, function (data) {
+        // console.log('got it', data);
+    });
 });
--- server/server.js
+++ server/server.js
@@ -16,15 +16,18 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
     console.log('New user connected');
-
+    
+    // Event emit
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
 
     socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    // Event Listenner 
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         const { from, text, createdAt } = message;
         io.emit('newMessage',generateMessage(from, text));
+        callback('This is from the server');
     });
     
     socket.on('disconnect', () => {
