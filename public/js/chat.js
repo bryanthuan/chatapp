e91f168a3889989d790a0512d3ded4f00c14ee86
Brian Nguyen <thuann89@gmail.com>
2017-09-03 16:37:28 +0700

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,20 +17,14 @@ socket.on('newMessage', function (message) {
     wrap.appendChild(messageElm);
 });
 
-// socket.emit('createMessage', {
-//     from: 'Frank',
-//     text: 'Hello'
-// }, function (data) {
-//     console.log('got it', data);
-// });
-
 document.getElementById('message-form').addEventListener('submit',function (e) {
     e.preventDefault();
     var text = document.getElementById('messageInput').value;
     socket.emit('createMessage', {
         from: 'User',
         text
-    }, function (data) {
+    }, function () {
+        document.getElementById('messageInput').value = '';
         // console.log('got it', data);
     });
 });
--- server/server.js
+++ server/server.js
@@ -5,10 +5,11 @@ const socketIO = require('socket.io');
 
 const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
-const server = http.createServer(app);
+const server = http.Server(app);
 const io = socketIO(server);
 
 //Config middleware to use static file
@@ -17,9 +18,10 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
     
-    // Event emit
+    // Emit newMessage event to client
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
 
+    
     socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
 
     // Event Listenner 
