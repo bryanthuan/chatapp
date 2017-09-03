cc300c78d1f570c42ad74b7380674374dcb07f2a
Brian Nguyen <thuann89@gmail.com>
2017-09-03 08:36:48 +0700

    Add new message and create message event

--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+//Config middleware to use static file
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: "thuann89@gmail.com",
+        text: "I am Brian Thuan, I have just sent something out to client",
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
+
