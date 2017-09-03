cd6c9d28561280664b54b79bbdbee20cebf292ec
Brian Nguyen <thuann89@gmail.com>
2017-09-03 09:24:42 +0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,12 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: "thuann89@gmail.com",
-        text: "I am Brian Thuan, I have just sent something out to client",
-        createdAt: new Date().getTime()
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        const { from, text, createdAt } = message;
+        io.emit('newMessage',{
+            from, text, createdAt
+        })
     });
     
     socket.on('disconnect', () => {
